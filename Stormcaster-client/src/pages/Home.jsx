eb4cede515c2878f697655c0f6ce2f932a821d6d
Brian Blankenship <brian.ga.edu@gmail.com>
2020-11-29 15:22:00 -0600

    Home page will display currentWeather.

--- Stormcaster-client/src/pages/Home.jsx
+++ Stormcaster-client/src/pages/Home.jsx
@@ -3,13 +3,12 @@ import { Container } from '@material-ui/core';
 
 import CurrentForecast from '../components/weather/CurrentForecast';
 
-
 const Home = () => {
-  return (
-    <Container maxWidth="xl">
-      <CurrentForecast />
-    </Container>
-  );
-}
+    return (
+        <Container maxWidth="xl">
+            <CurrentForecast />
+        </Container>
+    );
+};
 
 export default Home;
