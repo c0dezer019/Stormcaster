3c88a9390e80b6162c4a5fe8e6815990a1829b25
Brian Blankenship <brian.ga.edu@gmail.com>
2020-11-29 22:50:10 -0600

    Now retrieves all relevant weather data.

--- Stormcaster-client/src/components/weather/WeatherProvider.jsx
+++ Stormcaster-client/src/components/weather/WeatherProvider.jsx
@@ -1,30 +1,34 @@
-import React, { useState, useEffect } from 'react';
-// import PropTypes from 'prop-types';
+import { useContext, useEffect } from 'react';
+import { LocaleContext } from '../../state/LocaleContext';
+import { WeatherContext } from '../../state/WeatherContext';
 
 const WeatherProvider = () => {
-    // const { locationData } = props;
-    const [weatherData, setWeatherData] = useState(null);
-    // const [locales, setLocales] = useState({});
+    const { coords, firstRender } = useContext(LocaleContext);
+    const { setWeatherData } = useContext(WeatherContext);
 
-    useEffect(() => {
-        fetch(
-            `${process.env.REACT_APP_OWR_URL}/onecall?lat=33.1071&lon=98.5895&appid=${process.env.REACT_APP_OWR_KEY}`
-        )
-            .then(res => res.json())
-            .then(result => {
-                setWeatherData(result);
-            });
-    }, []);
+    useEffect(async () => {
+        if (firstRender && coords !== '') {
+            const weatherData = await fetch(
+                `${process.env.REACT_APP_OWR_URL}/onecall?lat=${coords.lat}&lon=${coords.lng}&appid=${process.env.REACT_APP_OWR_KEY}`
+            ).then(res => res.json());
 
-	console.log(weatherData);
-	
-	return (
-		<div>This is the weather</div>
-	)
+            console.log(weatherData.results[0]);
+            setWeatherData(weatherData.results[0]);
+        }
+    }, [firstRender]);
+
+    useEffect(async () => {
+        if (!firstRender && coords !== '') {
+            const weatherData = await fetch(
+                `${process.env.REACT_APP_OWR_URL}/onecall?lat=${coords.lat}&lon=${coords.lng}&appid=${process.env.REACT_APP_OWR_KEY}`
+            ).then(res => res.json());
+
+            console.log(weatherData.results[0]);
+            setWeatherData(weatherData.results[0]);
+        }
+    }, [coords])
+
+    return null;
 };
-
-/* WeatherProvider.propTypes = {
-    locationData: PropTypes.object.isRequired,
-}; */
 
 export default WeatherProvider;
