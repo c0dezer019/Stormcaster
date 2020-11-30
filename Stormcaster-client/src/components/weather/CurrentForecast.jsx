d5a22fcd7fa2f89108164a7d38d9a2623f4e4fbc
Brian Blankenship <brian.ga.edu@gmail.com>
2020-11-30 13:35:55 -0600

    Retrieve weather data for current area.

--- Stormcaster-client/src/components/weather/CurrentForecast.jsx
+++ Stormcaster-client/src/components/weather/CurrentForecast.jsx
@@ -1,11 +1,31 @@
-import React from 'react';
-import LocaleConversion from '../LocaleConversion';
+import React, { useContext, useEffect } from 'react';
+import { SuperContext } from '../../state/SuperContext';
 
 const CurrentForecast = () => {
-    // const searchContext = useContext(SearchContext);
+    const { setWeatherData, weatherData, coords } = useContext(SuperContext);
+
+    const fetchWeatherData = async () => {
+        const wthrData = await fetch(
+            `${process.env.REACT_APP_OWR_URL}/onecall?lat=${coords.lat}&lon=${coords.lng}&appid=${process.env.REACT_APP_OWR_KEY}`
+        ).then(res => res.json());
+
+        return wthrData;
+    };
+
+    useEffect(async () => {
+        const data = await fetchWeatherData();
+        setWeatherData(data);
+    }, []);
+
+    useEffect(() => {
+        if (weatherData !== '') {
+            console.log(weatherData);
+        }
+    }, [weatherData]);
+
     return (
         <div>
-            <LocaleConversion />
+            <h1>Hello</h1>
         </div>
     );
 };
