eadbc0a6d4030fe52a643228ceb66b5d445f8654
Brian Blankenship <brian.ga.edu@gmail.com>
2020-11-30 16:16:59 -0600

    Changed fetch methods to axios

--- Stormcaster-client/src/components/weather/CurrentForecast.jsx
+++ Stormcaster-client/src/components/weather/CurrentForecast.jsx
@@ -1,23 +1,33 @@
 import React, { useContext, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { dJAPI, oWAPI } from '../../config/axios';
 import { SuperContext } from '../../state/SuperContext';
 
-const CurrentForecast = () => {
-    const { setWeatherData, weatherData, coords } = useContext(SuperContext);
+const CurrentForecast = props => {
+    const { coords } = props;
+    const { setCurrentMessage, setWeatherData, weatherData } = useContext(SuperContext);
 
     const fetchWeatherData = async () => {
-        const wthrData = await fetch(
-            `${process.env.REACT_APP_OWR_URL}/onecall?lat=${coords.lat}&lon=${coords.lng}&appid=${process.env.REACT_APP_OWR_KEY}`
-        ).then(res => res.json());
+        const wthrData = await oWAPI(`/onecall?lat=${coords.lat}&lon=${coords.lng}&appid=${process.env.REACT_APP_OWR_KEY}`);
 
         return wthrData;
     };
 
+    const fetchMessage = async () => {
+        const msgData = await dJAPI();
+        console.log(msgData);
+    } 
+
     useEffect(async () => {
-        const data = await fetchWeatherData();
-        setWeatherData(data);
+        if (Object.keys(coords).length !== 0) {
+            const data = await fetchWeatherData();
+            setWeatherData(data);
+        }
+        const msg = await fetchMessage();
+        setCurrentMessage(msg);
     }, []);
 
-    useEffect(() => {
+    useEffect( async () => {
         if (weatherData !== '') {
             console.log(weatherData);
         }
@@ -29,5 +39,9 @@ const CurrentForecast = () => {
         </div>
     );
 };
+
+CurrentForecast.propTypes = {
+    coords: PropTypes.object.isRequired,
+}
 
 export default CurrentForecast;
