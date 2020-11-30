bd02cbf64a7ed1b2c75a0a269516ab57d01db938
Brian Blankenship <brian.ga.edu@gmail.com>
2020-11-29 22:44:10 -0600

    Home page will geolocate user on first render and upon return to homepage.

--- Stormcaster-client/src/pages/Home.jsx
+++ Stormcaster-client/src/pages/Home.jsx
@@ -1,11 +1,64 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 import { Container } from '@material-ui/core';
-
+import WeatherProvider from '../components/weather/WeatherProvider';
+import { LocaleContext } from '../state/LocaleContext';
 import CurrentForecast from '../components/weather/CurrentForecast';
 
 const Home = () => {
+    const { coords, setCoords, firstRender, setFirstRender } = useContext(LocaleContext);
+    const location = useLocation();
+
+    let isGeolocationSupported = false;
+
+    const setPosition = position => {
+        setCoords({
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+        });
+    };
+
+    const getLocation = () => {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(setPosition);
+            isGeolocationSupported = true;
+        } else {
+            isGeolocationSupported = false;
+        }
+    };
+
+    useEffect(() => {
+        if (coords === '' && firstRender) {
+            getLocation();
+            setFirstRender(false)
+            console.log(isGeolocationSupported);
+        }
+    }, []);
+
+    useEffect(() => {
+        setFirstRender(true);
+    }, [location.pathname]);
+
     return (
         <Container maxWidth="xl">
+            {isGeolocationSupported ? (
+                <>
+                    <p>
+                        Geolocation is not supported by this browser, please
+                        supply you location manually or enable it if it is
+                        turned off to get automatic current weather for current
+                        location.
+                    </p>
+                </>
+            ) : (
+                <>
+                    <p>
+                        lat: {coords.lat} lon: {coords.lng}
+                    </p>
+                </>
+            )}
+            <LocaleContext />
+            <WeatherProvider />
             <CurrentForecast />
         </Container>
     );
