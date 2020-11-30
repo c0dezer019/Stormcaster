9d60c2982bf02fb038d18bbce0875e587ff5ed82
Brian Blankenship <brian.ga.edu@gmail.com>
2020-11-29 22:45:13 -0600

    Added new Provider.

--- Stormcaster-client/src/App.jsx
+++ Stormcaster-client/src/App.jsx
@@ -3,13 +3,16 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import Routes from './config/routes';
 import UserModel from './models/user';
-import { AppContext } from './state/AppContext';
+import { LocaleContext } from './state/LocaleContext';
+import { WeatherContext } from './state/WeatherContext';
 
 function App() {
     const [currentUser, setCurrentUser] = useState(localStorage.getItem('id'));
     const [query, setQuery] = useState('');
     const [coords, setCoords] = useState('');
-    const value = {query, setQuery, coords, setCoords };
+    const [weatherData, setWeatherData] = useState({});
+    const value = { query, setQuery, coords, setCoords, firstRender, setFirstRender };
+    const wthrValue = { weatherData, setWeatherData };
 
     const storeUser = userId => {
         setCurrentUser({ currentUser: userId });
@@ -28,10 +31,12 @@ function App() {
 
     return (
         <div className="App">
-            <AppContext.Provider value={value}>
-                <Header currentUser={currentUser} logout={logout} />
-                <Routes currentUser={currentUser} storeUser={storeUser} />
-            </AppContext.Provider>
+            <LocaleContext.Provider value={value}>
+                <WeatherContext.Provider value={wthrValue}>
+                    <Header currentUser={currentUser} logout={logout} />
+                    <Routes currentUser={currentUser} storeUser={storeUser} />
+                </WeatherContext.Provider>
+            </LocaleContext.Provider>
             <Footer />
         </div>
     );
