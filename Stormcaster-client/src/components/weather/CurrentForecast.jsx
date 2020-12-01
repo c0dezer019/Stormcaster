a4754bc5ad75a2a33d3aa4cf95b2d3b144d9a338
Brian Blankenship <brian.ga.edu@gmail.com>
2020-12-01 11:26:04 -0600

    Added axios calls and instances.

--- Stormcaster-client/src/components/weather/CurrentForecast.jsx
+++ Stormcaster-client/src/components/weather/CurrentForecast.jsx
@@ -1,47 +1,47 @@
-import React, { useContext, useEffect } from 'react';
-import PropTypes from 'prop-types';
+import React, { useEffect, useState } from 'react';
+import { Container } from '@material-ui/core';
+import Summary from '../subcomponents/weather_components/Summary';
+import '../../css/currentWeather.css'
+
 import { dJAPI, oWAPI } from '../../config/axios';
-import { SuperContext } from '../../state/SuperContext';
 
 const CurrentForecast = props => {
     const { coords } = props;
-    const { setCurrentMessage, setWeatherData, weatherData } = useContext(SuperContext);
+    const [weatherData, setWeatherData] = useState({});
+    const [message, setMessage] = useState('');
 
     const fetchWeatherData = async () => {
-        const wthrData = await oWAPI(`/onecall?lat=${coords.lat}&lon=${coords.lng}&appid=${process.env.REACT_APP_OWR_KEY}`);
+        const wthrData = await oWAPI({
+            url: `/onecall?lat=${coords.lat}&lon=${coords.lng}&units=imperial&appid=${process.env.REACT_APP_OWM_KEY}`
+        });
 
         return wthrData;
     };
 
     const fetchMessage = async () => {
         const msgData = await dJAPI();
-        console.log(msgData);
+
+        return msgData;
     } 
 
     useEffect(async () => {
         if (Object.keys(coords).length !== 0) {
             const data = await fetchWeatherData();
-            setWeatherData(data);
+            setWeatherData(data.data);
         }
         const msg = await fetchMessage();
-        setCurrentMessage(msg);
-    }, []);
+        setMessage(msg.data.joke);
+    }, [coords]);
 
-    useEffect( async () => {
-        if (weatherData !== '') {
-            console.log(weatherData);
-        }
-    }, [weatherData]);
+    if (!weatherData.current) {
+        return null;
+    } 
 
-    return (
-        <div>
-            <h1>Hello</h1>
-        </div>
+    return ( 
+        <Container maxWidth="xl">
+            <Summary weatherData={weatherData} msg={message} />
+        </Container>
     );
 };
-
-CurrentForecast.propTypes = {
-    coords: PropTypes.object.isRequired,
-}
 
 export default CurrentForecast;
--- Stormcaster-client/src/config/axios.jsx
+++ Stormcaster-client/src/config/axios.jsx
@@ -1,7 +1,12 @@
 import axios from 'axios';
 
 export const dJAPI = axios.create({
-    baseURL: `${process.env.REACT_APP_DJOKES}`,
 	method: 'get',
+    baseURL: `${process.env.REACT_APP_DJOKES}`,
 	headers: {'User-Agent': 'Stormcaster', 'Accept': 'application/json'}
+});
+
+export const oWAPI = axios.create({
+	method: 'get',
+	baseURL: `${process.env.REACT_APP_OWM_URL}`
 });
