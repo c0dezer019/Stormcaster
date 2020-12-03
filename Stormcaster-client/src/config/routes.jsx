e8688134527baa5ff22ea98a6e5d91befc9e6341
Brian Blankenship <brian.ga.edu@gmail.com>
2020-12-03 16:09:44 -0600

    Add context to routes.

--- Stormcaster-client/src/config/routes.jsx
+++ Stormcaster-client/src/config/routes.jsx
@@ -1,24 +1,34 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import Home from '../pages/Home';
 import Profile from '../pages/Profile';
 import FormContainer from '../containers/FormContainer';
 import Login from '../pages/Login';
+import { SuperContext } from '../state/SuperContext';
 
-const Routes = ({ currentUser, storeUser}) => (
-	<Switch>
-		<Route exact path="/" component={Home} />
-		<Route path="/profile" component={Profile} />
-		<Route path="/register" component={FormContainer} />
-		<Route path="/login" render={routeCProps => {
-			return <Login
-						{...routeCProps}
-						currentUser={currentUser}
-						storeUser={storeUser}
-					/>
-		}} />
-	</Switch>
-);
+const Routes = ({ storeUser }) => {
+    const { currentUser } = useContext(SuperContext);
+	
+	return (
+        <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/profile" component={Profile} />
+            <Route path="/register" component={FormContainer} />
+            <Route
+                path="/login"
+                render={routeCProps => {
+                    return (
+                        <Login
+                            {...routeCProps}
+                            currentUser={currentUser}
+                            storeUser={storeUser}
+                        />
+                    );
+                }}
+            />
+        </Switch>
+    );
+};
 
 export default Routes;
