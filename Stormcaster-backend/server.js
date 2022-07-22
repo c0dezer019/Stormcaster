bcbcd69536f84eafe635521a3ca1bd782c365abb
Brian Blankenship <c0dezer019@zohomail.com>
2022-07-22 01:18:22 -0500

    Minor stuff.

--- Stormcaster-backend/server.js
+++ Stormcaster-backend/server.js
@@ -2,17 +2,18 @@
 require('dotenv').config();
 require(__dirname + '/config/config.js')[process.env.DB_PASS];
 const express = require('express');
+const session = require('express-session');
 const routes = require('./routes');
 const cors = require('cors');
 const csrf = require('csurf');
 const cookieParser = require('cookie-parser');
 const passport = require('./passport');
+const flash = require('connect-flash');
 
 const port = process.env.PORT || 4000;
 const app = express();
 
 // middleware
-app.use(express.json());
 
 const corsOptions = {
 	// from which URLs do we want to accept requests
@@ -22,10 +23,25 @@ const corsOptions = {
 };
 const csrfProtection = csrf({ cookie: true });
 
+const sess = 
+
 app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(session({
+	cookie: {
+		secure: true,
+		sameSite: true,
+		maxAge: 1000 * 60 * 60 * 24,
+	},
+	resave: false,
+	saveUninitialized: false,
+	secret: process.env.SESSION_SECRET,
+}));
 app.use(cors(corsOptions));
 app.use(passport.initialize());
 app.use(passport.session());
+app.use(flash());
 
 // middleware for logged in user
 app.use(csrfProtection, (req, res, next) => {
